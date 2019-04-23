46caf9ad204b2a83bb4e32af85b5982ef477e362
hardlyrichie <hardlyrichie@gmail.com>
2019-04-23 12:20:05 -0400

    Add generate world

--- generate.py
+++ generate.py
@@ -8,15 +8,22 @@ from world import ATTRIBUTES, OBJECT_TYPES, ITEMS, ROOMS
 
 def write_attributes():
     """
-    Writes item attributes to file
+    Writes item attributes to /content/attributes.json 
     """
     with open_w('content/attritubes.json') as f:
         json.dump(ATTRIBUTES, f)
 
 
 def generate_items(n):
-    """
-    Generates n items to items.json, describing the item attributes and actions
+    """Generates n items and writes to /content/items.json
+    
+    Items contains list of attributes that describe their properties. 
+
+    Args:
+        n (int): Number of unique items to generate
+
+    Returns:
+        Dictionary of generated items
     """
     items = defaultdict(dict)
 
@@ -31,35 +38,52 @@ def generate_items(n):
                 attributes += OBJECT_TYPES[p]
             elif p in ATTRIBUTES:
                 attributes.append(p)
-                
+
             items[item] = attributes
 
     # Write items to a json file
     with open_w('content/items.json') as f:
         json.dump(items, f)
+    
+    return items
 
 
-def generate_rooms(n):
-    """
-    Generates n rooms to rooms.json, describing the items in room and the navigation
+def generate_rooms(n, items):
+    """Generates n rooms and writes to /content/rooms.json 
+    
+    Rooms describe what items are in it and what other rooms (directions) are possible to navigate to.
+
+    Args:
+        n (int): Number of unique rooms to generate
+        items (dict): Dictionary of possible unique items in the world
     """
     rooms = defaultdict(dict)
 
     # Choose n unique rooms
     room_rand = random.sample(ROOMS, n)
 
-    # TODO: Make rooms direction similar across all rooms to navigate
+    # TODO: Make rooms direction similar across all rooms to navigate. Random walk generation
     for room in room_rand:
         # Choose n-1 or 4 other rooms to navigate to
         other_rooms = random.sample(list(filter(lambda r: r != room, room_rand)), n-1 if n-1 < 4 else 4)
 
-        # TODO: Make items picked contexual
-        item = random.sample(list(ITEMS.keys()), random.randint(0, len(ITEMS)-1))
+        # TODO: Make items placed in rooms contexual, Not enough items? 
+        item = random.sample(list(items.keys()), random.randint(0, len(items)-1))
         rooms[room] = { "directions": other_rooms, "items": item }
 
     with open_w('content/rooms.json') as f:
         json.dump(rooms, f)
 
+def generate_world(i, r):
+    """Generates the world environment (items, rooms)
+    
+    Args:
+        i (int): Number of unique items to generate
+        r (int): Number of unique rooms to generate
+    """
+    items = generate_items(i)
+    generate_rooms(r, items)
+    
 
 def mkdir(path):
     """
@@ -81,5 +105,4 @@ def open_w(path):
 
 if __name__ == "__main__":
     write_attributes()
-    generate_items(5)
-    generate_rooms(3)
+    generate_world(5, 3)
