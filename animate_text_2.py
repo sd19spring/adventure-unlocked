0d25174701beacd8d28c13beffcf009a8a3bf3ed
Colin Takeda <ctakeda@olin.edu>
2019-05-01 17:34:48 -0400

    Text output and input integrated

--- animate_text_2.py
+++ animate_text_2.py
@@ -5,7 +5,7 @@ Secondary test script to work on a different approach to outputting text
 import pygame as pg
 import time
 
-
+pg.init
 
 class Terminal():
     """
@@ -22,18 +22,16 @@ class Terminal():
     def render_multi_line(self,x,y,fsize,screen,font):
         #lines = text.splitlines()
         for i, l in enumerate(self.text):
-            print('YES')
-            time.sleep(1)
+            #print('YES')
             txt_surf = font.render(l, 0, (255,0,0))
             txtRect = txt_surf.get_rect()
             txtRect.x = x
             txtRect.y = y + fsize*i
             screen.blit(txt_surf, txtRect)
 
-
 def main():
 
-    pg.init()
+    # pg.init()
     terminal = Terminal()
     pg.font.init()
 
@@ -42,6 +40,10 @@ def main():
     color = (0,0,255)
     txtcolor = (0,0,0)
     fontsize = 20
+    input_box = pg.Rect(100, 100, 140, 32)
+    color_inactive = pg.Color('lightskyblue3')
+    color_active = pg.Color('dodgerblue2')
+
 
     X = 640
     Y = 480
@@ -49,24 +51,54 @@ def main():
     screen = pg.display.set_mode((X, Y))
     pg.display.set_caption('Adventure Unlocked')
     font = pg.font.SysFont(None, fontsize)
-
+    text = ''
+    active = False
     running = True
 
     while running:
 
-
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                pg.quit()
+                done = True
+            if event.type == pg.MOUSEBUTTONDOWN:
+                # If the user clicked on the input_box rect.
+                if input_box.collidepoint(event.pos):
+                    # Toggle the active variable.
+                    active = not active
+                else:
+                    active = False
+                # Change the current color of the input box.
+                color = color_active if active else color_inactive
+            if event.type == pg.KEYDOWN:
+                if active:
+                    if event.key == pg.K_RETURN:
+                        terminal.update(text)
+                        text = ''
+                    elif event.key == pg.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        text += event.unicode
 
-        screen.fill((255,0,255))
-        print(X,Y,fontsize,screen)
+        screen.fill((255,255,255))
+
+        txt_surface = font.render(text, True, color)
+        # Resize the box if the text is too long.
+        width = max(200, txt_surface.get_width()+10)
+        input_box.w = width
+        # Blit the text.
+        screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
+        # Blit the input_box rect.
+        pg.draw.rect(screen, color, input_box, 2)
+
+
+        # screen.fill((255,255,255))
+        #print(X,Y,fontsize,screen)
         terminal.render_multi_line(24,24,fontsize,screen,font)
-        terminal.update(input("Update list:"))
+        #terminal.update(input("Update list:"))
 
-        pg.display.update()
-        #pg.display.flip()
-        # clock.tick(60)
+#        pg.display.update()
+        pg.display.flip()
+        clock.tick(60)
 
 if __name__ == '__main__':
     main()
